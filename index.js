8dd36196cb5d1cbef8ecf9834130644b0a0e4b8b
Samuel <smarouba2@gmail.com>
2022-06-11 18:11:56 -0400

    commit tree 8f5080bc9ab8a354247b2e1dcb5cadeab9a4021a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(element => element.result === "W")
+    if (win) {
+     return win.year
+    }else return undefined
+}
+
+//function takes a record array
+//element of key & value of year and result 
+//uses .find to test each object (element.find.record.year)
+//
